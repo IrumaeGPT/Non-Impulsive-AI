786a234aa25a0bf6dc0210c5200e266070d8b59e
Lse-Yeong <ocdee39@gmail.com>
2024-08-20 12:48:10 +0900

    feat:프로젝트 초기 설정

---
+++ model/User.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    userId: str
+    timestamp: int
+    observation: str
+    importance: float
+    isEventScene: bool
+    reasonIds: str
+    
+class User(BaseModel):
+    userId: str
