b6b0dc1a66908d11e7182446c9a162e266990e42
Lse-Yeong <ocdee39@gmail.com>
2024-09-26 04:29:47 +0900

    feat: 클러스터 CRUD 구현 완료

--- episodeManager/api/episodeManager.py
+++ episodeManager/api/episodeManager.py
@@ -1,15 +1,11 @@
-'''
-1. Episode Memory 에서 해당 episode id 에 해당하는 기억 가지고 오기
-2. 기억 추가하기 
-
-'''
-
 from fastapi import APIRouter
 import chromadb
 from model.episodeItem import episodeItem
 from model.queryItem import queryItem
 from model.updateEpisodeItem import updateEpisodeItem
 import embedding_model.modelUpload as modelUpload
+import numpy as np
+from sklearn.cluster import KMeans
 
 client=chromadb.PersistentClient()
 
@@ -17,6 +13,31 @@ embed_model=modelUpload.model_upload()
 
 episodeRouter = APIRouter()
 
+cluster = []
+
+def updateCluster(cluster_result, embeddings):
+    global cluster
+    cluster = [{"clusterId":0, "sum": [0 for _ in range(768)],"count":0, "avg":[0 for _ in range(768)]}]
+    for idx in range(len(cluster_result)):
+        isExist=False
+        for i in range(len(cluster)):
+            if((cluster[i])["clusterId"]==cluster_result[idx]):
+                isExist=True
+                for j in range(768):
+                    ((cluster[i])["sum"])[j]+=(embeddings[idx])[j]
+                (cluster[i])["count"]+=1
+                break
+            else:
+                continue
+        if(not isExist):
+            cluster.append({"clusterId":cluster_result[idx], "sum": embeddings[idx] , "count":1 ,"avg": [0 for _ in range(768)]})
+    
+    for idx in range(len(cluster)):
+        for j in range(768):
+            ((cluster[idx])["avg"])[j] = ((cluster[idx])["sum"])[j] / ((cluster[idx])["count"])
+    
+    return
+            
 @episodeRouter.post("/episode/add")
 async def saveQueryInShortTermMemory(episodeItem: episodeItem):
     userId = episodeItem.userId
@@ -52,7 +73,7 @@ async def getShortTermMemorys(userId : str):
     resultString=""
     
     if(n_result==0):
-        return 410
+        return []
     elif(n_result==1):
         n_result=1
         
@@ -73,33 +94,112 @@ async def getShortTermMemorys(userId : str):
     return resultString
     
 
-@episodeRouter.patch("/episode/update/")
+@episodeRouter.patch("/episode/update")
 async def updateEpisodeMemory(updateEpisodeItem : updateEpisodeItem):
-    # epiosdeEmbedding=[]
+    epiosdeEmbedding=[]
     
-    # userId = updateEpisodeItem.userId
-    # summary = updateEpisodeItem.summary
-    # collection=client.get_collection(name=userId+"_episode")
+    userId = updateEpisodeItem.userId
+    summary = updateEpisodeItem.summary
+    collection=client.get_collection(name=userId+"_episode")
     
-    # #Episode 다 빼고
-    # n_result = collection.count()
-    # if(n_result==0):
-    #     #최초 클러스터 형성하기
-    #     _=3
-    #     return
-    # query_embedding_word=[" "]
-    # query_embedding = embed_model.encode(query_embedding_word)
-    # query_embedding=query_embedding.tolist()
+    #Episode 다 빼고
+    n_result = collection.count()
+    if(n_result==0):
+        #최초 클러스터 형성하기
+        print("Hello")
+        id=str(1)
+        
+        metadatas=[
+            {"id": id, "userId":userId, "summary" : summary, "cluster": 0 }
+        ]
+        
+        summary_embedding_word=[summary]
+        summary_embedding = embed_model.encode(summary_embedding_word)
+        summary_embedding=summary_embedding.tolist()
+        print(summary_embedding)
+        
+        collection.add(
+            ids=id,
+            metadatas=metadatas,
+            embeddings=summary_embedding
+        )
+        
+        updateCluster([0], summary_embedding)
+        print(cluster)
     
-    # result=collection.query(
-    #     query_embeddings=query_embedding[0],
-    #     n_results=n_result,
-    # )
+        return 
     
+    query_embedding_word=[" "]
+    query_embedding = embed_model.encode(query_embedding_word)
+    query_embedding=query_embedding.tolist()
+    
+    result=collection.query(
+        query_embeddings=query_embedding[0],
+        n_results=n_result,
+    )
+    
+    metas=(result["metadatas"])[0]
+    metas.sort(key= lambda x: int(x["id"]))
+    
+    for item in metas:
+        print(item["summary"])
+        item_embedding = embed_model.encode([item["summary"]])
+        item_embedding = item_embedding.tolist()
+        epiosdeEmbedding.append(item_embedding[0])
+    
+    summary_embedding_word=[summary]
+    summary_embedding = embed_model.encode(summary_embedding_word)
+    summary_embedding=summary_embedding.tolist()
+    epiosdeEmbedding.append(summary_embedding[0])
+    
+    print(epiosdeEmbedding[0][0]-epiosdeEmbedding[1][0])
+    
+    # K-Means 클러스터링
+    k = 4  # 클러스터 개수
+    kmeans = KMeans(n_clusters=k)
+    kmeans.fit(epiosdeEmbedding)
+
+    # 클러스터 레이블과 중심점
+    cluster_labels = kmeans.labels_
+    print(cluster_labels)
+    
+    for idx in range(0,len(metas)):
+        if(cluster_labels[idx]!=(metas[idx])["cluster"]):
+            collection.delete(ids=[(metas[idx])["id"]])
+            
+            updated_metadata = {"id": (metas[idx])["id"], "userId": (metas[idx])["userId"], "summary" : (metas[idx])["summary"], "cluster": int(cluster_labels[idx]) }
+
+            collection.add(
+                ids=(metas[idx])["id"],  # 새로운 ID
+                embeddings=epiosdeEmbedding[idx],  # 예시 벡터
+                metadatas=updated_metadata  # 수정된 메타데이터 사용
+            )
+    
+    id=str(get_ids_max(collection)+1)
+    
+    metadatas=[
+            {"id": id, "userId":userId, "summary" : summary, "cluster": int(cluster_labels[-1])}
+    ]
+    
+    print("zzz")
+    
+    result = collection.add(
+        ids=id,
+        metadatas=metadatas,
+        embeddings=summary_embedding
+    )
+    
+    result = collection.query(
+        query_embeddings=query_embedding[0],
+        n_results=n_result+1
+    )
+    
+    updateCluster(cluster_labels , epiosdeEmbedding)
+    print(cluster)
     #클러스터링 하고
     #클러스터 번호 업데이트
 
-    return
+    return (result["metadatas"])[0]
     
 @episodeRouter.post("/episode/retrieve")
 async def getEpisode(queryitem : queryItem):
@@ -133,14 +233,59 @@ async def getEpisode(queryitem : queryItem):
     
     
     ## 버전 2
-    queryStringList=queryitem.querys
+    query=queryitem.query
     userId=queryitem.userId
     
     collection=client.get_collection(name=userId+"_episode")
+    n_result = collection.count()
+    result_response=[]
     
+    query_word=[query]
+    query_word_embedding = embed_model.encode(query_word)
+    query_word_embedding = query_word_embedding.tolist()
+    cosine_result = []
     
-     
-    return 
+    for i in range(len(cluster)):
+        cluster_avg = (cluster[i])["avg"]
+        cosine_result.append([(cluster[i]["clusterId"]),cosine_similarity(query_word_embedding[0],cluster_avg)])
+    cosine_result.sort(key=lambda x: x[1], reverse=True)
+    
+    print(cosine_result)
+    max_cluster = (cosine_result[0])[0]
+    
+    query_embedding_word=[" "]
+    query_embedding = embed_model.encode(query_embedding_word)
+    query_embedding=query_embedding.tolist()
+    
+    result=collection.query(
+        query_embeddings=query_embedding[0],
+        n_results=n_result,
+    )
+    
+    metas=(result["metadatas"])[0]
+    
+    for item in metas:
+        if(item["cluster"]==max_cluster):
+            result_response.append(item["summary"])
+    
+    return result_response
+
+def cosine_similarity(list1, list2):
+    # 리스트를 numpy 배열로 변환
+    vec1 = np.array(list1)
+    vec2 = np.array(list2)
+    
+    # 두 벡터의 내적 계산
+    dot_product = np.dot(vec1, vec2)
+    
+    # 벡터의 크기(노름) 계산
+    norm_vec1 = np.linalg.norm(vec1)
+    norm_vec2 = np.linalg.norm(vec2)
+    
+    # 코사인 유사도 계산
+    cosine_sim = dot_product / (norm_vec1 * norm_vec2)
+    
+    return cosine_sim
 
     #id 최대값을 int로 반환
 def get_ids_max(collection):
--- episodeManager/model/queryItem.py
+++ episodeManager/model/queryItem.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel
 
 class queryItem(BaseModel):
     userId : str
-    querys : list
+    query : str
